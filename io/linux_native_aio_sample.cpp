700c255e936011cc18e5af16dafadd648f037ed4
Deepliu <me@liudonghua.net>
2020-07-17 10:24:28 +0800

    native aio sample

--- io/linux_native_aio_sample.cpp
+++ io/linux_native_aio_sample.cpp
@@ -4,7 +4,6 @@
 #include<errno.h>
 #include<stdio.h>
 #include<unistd.h>
-//#define __USE_GNU 1
 #include<fcntl.h>
 
 #define FILEPATH "./test.txt"
@@ -12,31 +11,32 @@
 int main()
 {
     io_context_t context;
-    unsigned  nr_events = 10;
-
     struct iocb io[2], *p[2]={&io[0], &io[1]};
     struct io_event e[2];
+    unsigned nr_events = 10;
+
     struct timespec timeout;
-    char* wbuf = 0;
-    char* rbuf = 0;
-    //int wbuflen = 512*1024*1024;
-    int wbuflen = 1024*1024;
+    timeout.tv_sec = 0;
+    timeout.tv_nsec = 10000000;
+
+    char *wbuf = 0, *rbuf = 0;
+    int wbuflen = 512*1024*1024;
     int rbuflen = wbuflen+1;
-    int ret = 0;
-    int comp_num = 0;
-    int i = 0;
     posix_memalign((void**)&wbuf, 512, wbuflen);
     posix_memalign((void**)&rbuf, 512, rbuflen);
     memset(wbuf, 'a', wbuflen);
     memset(rbuf, 0, rbuflen);
-    // memset(buf, 'a', buflen);
+    memset(&context, 0, sizeof(io_context_t));
+
+    int ret = 0, comp_num = 0, i = 0;
+
     int fd = open(FILEPATH, O_CREAT|O_RDWR|O_DIRECT, 0644);
-    if(fd < 0){ 
+    if(fd < 0)
+	{ 
         printf("open file failed ！\n");
         return 0;
     }   
 
-    memset(&context, 0, sizeof(io_context_t));
     if( 0 != io_setup(nr_events, &context) ){
 
         printf("io_setup error:%d\n", errno);
@@ -52,9 +52,8 @@ int main()
         return -1;
     }
 
-    while(1){
-        timeout.tv_sec = 0;
-        timeout.tv_nsec = 10000000;
+    while(true)
+	{
         ret = io_getevents(context,2,2,e,&timeout);
 
         if(ret < 0)
@@ -66,16 +65,18 @@ int main()
         if(ret > 0)
         {   
             comp_num += ret;
-            for( i = 0;i < ret; ++i){
+            for( i = 0;i < ret; ++i)
+			{
                 printf("result,res2:%d, res:%d\n", e[i].res2, e[i].res);
             }
         }
-        if(comp_num >= 2){
+
+        if(comp_num >= 2)
+		{
             printf("done !\n");
             break;
         }
         printf("have not done !\n");
-
     }
 
     return 0;
