7988af9f8f9bf76e6b0081309aa0f687d8e3c0ab
Deepliu <me@liudonghua.net>
2020-07-18 09:55:11 +0800

    tour of go

--- tour_of_go/concurrency.go
+++ tour_of_go/concurrency.go
@@ -2,6 +2,7 @@ package main
 import(
 	"fmt"
 	"time"
+	"sync"
 )
 
 func say(s string) {
@@ -41,9 +42,47 @@ func fibonacci1(c, quit chan int) {
 			return
 		default:
 			fmt.Println("chan not ready")
-			//time.Sleep(time.Millisecond)
+			time.Sleep(time.Millisecond)
 		}
 	}
+}
+
+type SafeCounter struct {
+	v map[string] int
+	mux sync.Mutex
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+func (c *SafeCounter)Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+// A首先被a阻塞，A()结束后关闭b，使b可读
+func A(a, b chan struct{}) {
+    <-a
+    fmt.Println("A()!")
+    time.Sleep(time.Second)
+    close(b)
+}
+
+// B首先被a阻塞，B()结束后关闭b，使b可读
+func B(a, b chan struct{}) {
+    <-a
+    fmt.Println("B()!")
+    close(b)
+}
+
+// C首先被a阻塞
+func C(a chan struct{}) {
+    <-a
+    fmt.Println("C()!")
 }
 
 func concurrency() {
@@ -79,5 +118,27 @@ func concurrency() {
 		quit <- 0
 	}()
 	fibonacci1(c2, quit)
+
+	c3 := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c3.Inc("key1")
+	}
+	//time.Sleep(time.Second)
+	fmt.Println(c3.Value("key1"))
+
+	// 指定goroutine执行顺序: A()-->B()-->C()
+	x1 := make(chan struct{})
+	y1 := make(chan struct{})
+	z1 := make(chan struct{})
+
+	go C(z1)
+	go A(x1, y1)
+	go C(z1)
+	go B(y1, z1)
+	go C(z1)
+
+	// 关闭x，让x可读
+	close(x1)
+	time.Sleep(3 * time.Second)
 }
 
